d4ecc7dc845a8f558b1d21ec531b15307f20a181
emoacht <emotom@pobox.com>
2015-01-26 12:26:13 +0900

    Changed OS version information

--- DesktopToast/Helper/OsVersion.cs
+++ DesktopToast/Helper/OsVersion.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Linq;
+using System.Management;
 
 namespace DesktopToast.Helper
 {
@@ -7,46 +9,6 @@ namespace DesktopToast.Helper
     /// </summary>
     internal class OsVersion
     {
-        #region Win32
-
-        [DllImport("kernel32", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool VerifyVersionInfoW(
-            [In] ref OSVERSIONINFOEX lpVersionInfo,
-            uint dwTypeMask,
-            ulong dwlConditionMask);
-
-        [DllImport("kernel32", SetLastError = true)]
-        private static extern ulong VerSetConditionMask(
-            ulong dwlConditionMask,
-            uint dwTypeBitMask,
-            byte dwConditionMask);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct OSVERSIONINFOEX
-        {
-            public uint dwOSVersionInfoSize;
-            public uint dwMajorVersion;
-            public uint dwMinorVersion;
-            public uint dwBuildNumber;
-            public uint dwPlatformId;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-            public string szCSDVersion;
-            public ushort wServicePackMajor;
-            public ushort wServicePackMinor;
-            public ushort wSuiteMask;
-            public byte wProductType;
-            public byte wReserved;
-        }
-
-        private static uint VER_MINORVERSION = 0x0000001;
-        private static uint VER_MAJORVERSION = 0x0000002;
-
-        private static byte VER_GREATER_EQUAL = 3;
-
-        #endregion
-
-
         /// <summary>
         /// Whether OS is Windows 8 or newer
         /// </summary>
@@ -56,32 +18,34 @@ namespace DesktopToast.Helper
             get
             {
                 if (!_isEightOrNewer.HasValue)
-                    _isEightOrNewer = IsEqualOrNewerByVerifyVersionInfo(6, 2);
+                {
+                    var ver = GetOsVersionByWmi();
+                    _isEightOrNewer = (ver != null) && (((6 == ver.Major) && (2 <= ver.Minor)) || (7 <= ver.Major));
+                }
 
-                return _isEightOrNewer.HasValue ? _isEightOrNewer.Value : false;
+                return _isEightOrNewer.Value;
             }
         }
         private static bool? _isEightOrNewer;
 
-        private static bool? IsEqualOrNewerByVerifyVersionInfo(int major, int minor)
+
+        #region Helper
+
+        private static Version GetOsVersionByWmi()
         {
-            var info = new OSVERSIONINFOEX();
-            info.dwMajorVersion = (uint)major;
-            info.dwMinorVersion = (uint)minor;
-            info.dwOSVersionInfoSize = (uint)Marshal.SizeOf(info);
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
 
-            ulong cm = 0;
-            cm = VerSetConditionMask(cm, VER_MAJORVERSION, VER_GREATER_EQUAL);
-            cm = VerSetConditionMask(cm, VER_MINORVERSION, VER_GREATER_EQUAL);
+            var os = searcher.Get().Cast<ManagementObject>().FirstOrDefault();
 
-            var result = VerifyVersionInfoW(ref info, VER_MAJORVERSION | VER_MINORVERSION, cm);
+            if ((os != null) && (os["OsType"] != null) && (os["Version"] != null))
+            {
+                if (os["OsType"].ToString() == "18") // WINNT
+                    return new Version(os["Version"].ToString());
+            }
 
-            if (result)
-                return true;
-
-            return (Marshal.GetLastWin32Error() == 1150) // ERROR_OLD_WIN_VERSION
-                ? false
-                : (bool?)null;
+            return null;
         }
+
+        #endregion
     }
 }
