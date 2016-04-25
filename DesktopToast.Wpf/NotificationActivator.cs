8b808a6f53c2e5f4ee4f6ff1615701e5e8a33383
emoacht <emotom@pobox.com>
2016-04-25 23:28:37 +0900

    Modified WPF sample

---
+++ DesktopToast.Wpf/NotificationActivator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopToast.Wpf
+{
+	/// <summary>
+	/// Inherited class of notification activator (for Action Center of Windows 10)
+	/// </summary>
+	/// <remarks>The CLSID of this class must be unique for each application.</remarks>
+	[Guid("f5b13fa4-8472-4f82-8a47-515b879006ba"), ComVisible(true), ClassInterface(ClassInterfaceType.None)]
+	[ComSourceInterfaces(typeof(INotificationActivationCallback))]
+	public class NotificationActivator : NotificationActivatorBase
+	{ }
+}
