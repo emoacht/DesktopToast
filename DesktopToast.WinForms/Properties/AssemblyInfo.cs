1ca16d2aaa6e58542e5cf1a07ff02164024be8c6
emoacht <emotom@pobox.com>
2016-04-26 01:21:09 +0900

    Incremented minor version

--- DesktopToast.WinForms/Properties/AssemblyInfo.cs
+++ DesktopToast.WinForms/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DesktopToast.WinForms")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 EMO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.2")]
-[assembly: AssemblyFileVersion("0.3.0.2")]
+[assembly: AssemblyVersion("0.3.0.3")]
+[assembly: AssemblyFileVersion("0.3.0.3")]
--- DesktopToast.Wpf/Properties/AssemblyInfo.cs
+++ DesktopToast.Wpf/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DesktopToast.Wpf")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 EMO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.2")]
-[assembly: AssemblyFileVersion("0.3.0.2")]
+[assembly: AssemblyVersion("0.4.0.4")]
+[assembly: AssemblyFileVersion("0.4.0.4")]
